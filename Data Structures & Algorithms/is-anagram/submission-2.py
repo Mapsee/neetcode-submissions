9cecbeaee3194ce8b7a15928c6115dd0ab9fde38
Nicole Leung <56210799+Mapsee@users.noreply.github.com>
2026-09-14 21:14:07 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        d = defaultdict(int)
+
+        for i in range(len(s)):
+            d[s[i]] += 1
+            d[t[i]] -= 1
+        
+        for count in d.values():
+            if count != 0:
+                return False
+
+        return True
+            
